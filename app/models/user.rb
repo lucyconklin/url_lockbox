4a3be2aa3bbeb04dae1e14c1a16acd7529ce6d76
Lucy Conklin <lucy.conklin@gmail.com>
2017-04-04 12:21:05 -0600

    Passing tests for Sign Up user story

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :links
+  validates :email, presence: true
   validates :email, uniqueness: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,5 +4,6 @@ RSpec.describe User, type: :model do
   context "relationships" do
     it { should have_many(:links) }
     it { should validate_uniqueness_of(:email) }
+    it { should validate_presence_of(:email) }
   end
 end

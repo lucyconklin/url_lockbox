8937261bfac4f54dc00b01753cc03eb936ca708d
Lucy Conklin <lucy.conklin@gmail.com>
2017-04-04 16:09:42 -0600

    Rip out custom url validation

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,19 +1,20 @@
 require 'uri'
 
 class Link < ActiveRecord::Base
-  validate :url_is_valid
-  validates :url, presence: true
-  validates :url, uniqueness: true
   validates :title, presence: true
+  validates :url, presence: true
+  # validate :url_is_valid
+  validates :url, uniqueness: true
 
   belongs_to :user
 
-  def url_is_valid
-    url = url || ''
-    if URI::parse(url).host != nil
-      return true
-    else
-      errors.add(:url, "is invalid")
-    end
-  end
+  # def url_is_valid
+  #   url ||= ' '
+  #   encoded_url = url.gsub(' ','%20')
+  #   if URI::parse(encoded_url).host != nil
+  #     return true
+  #   else
+  #     errors.add(:url, "is invalid")
+  #   end
+  # end
 end

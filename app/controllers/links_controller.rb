7605b64e888c0ac41db5ced2fc56501b680a061d
Lucy Conklin <lucy.conklin@gmail.com>
2017-04-04 15:23:51 -0600

    Url is validated through custom validation

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -2,5 +2,22 @@ class LinksController < ApplicationController
 
   def index
     @link = Link.new
+    @links = Link.all || ''
+  end
+
+  def create
+    link = current_user.links.new(link_params)
+    if link.save
+      redirect_to links_path
+    else
+      flash[:error] = link.errors.full_messages
+      redirect_to links_path
+    end
+  end
+
+  private
+
+  def link_params
+    params.require(:link).permit(:url, :title)
   end
 end
--- spec/features/links/user_can_submit_a_link_spec.rb
+++ spec/features/links/user_can_submit_a_link_spec.rb
@@ -18,5 +18,58 @@ describe "User Can Add Links" do
       expect(page).to have_content("Url:")
       expect(page).to have_content("Title:")
     end
+
+    it "I can save a valid link" do
+
+      fill_in 'link_url', with: "http://www.marthastewart.com"
+      fill_in 'link_title', with: "This is the best website"
+
+      click_on 'Submit'
+
+      expect(current_path).to eq(links_path)
+      expect(page).to have_content("http://www.marthastewart.com")
+      expect(page).to have_content("This is the best website")
+      expect(page).to have_content("Mark as read")
+    end
+
+    xit "I must fill out url field to submit link" do
+      fill_in 'link_title', with: "This is the best website"
+
+      click_on 'Submit'
+
+      expect(current_path).to eq(links_path)
+      expect(page).not_to have_content("This is the best website")
+      expect(Link.all.count).to eq(0)
+    end
+
+    xit "I must fill out title field to submit link" do
+    end
+
+    xit "I cannot save a link with an invalid url" do
+      fill_in 'link_url', with: "This is the best website"
+
+      click_on 'Submit'
+
+      expect(current_path).to eq(links_path)
+      expect(page).not_to have_content("This is the best website")
+      expect(Link.all.count).to eq(0)
+    end
+
+    xit "I cannot save a link without a unique url" do
+      expect(Link.all.count).to eq(0)
+      
+      link = Link.create(url: 'http://www.marthastewart.com',
+                         title: "no THIS is the best website",
+                         user_id: @user.id)
+
+      fill_in 'link_url', with: "http://www.marthastewart.com"
+      fill_in 'title', with: "something"
+
+      click_on 'Submit'
+
+      expect(current_path).to eq(links_path)
+      expect(page).not_to have_content("something")
+      expect(Link.all.count).to eq(1)
+    end
   end
 end

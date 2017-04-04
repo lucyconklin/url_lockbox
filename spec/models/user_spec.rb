bf70e2c2fa0d236b119a397adc652e417457e02e
Lucy Conklin <lucy.conklin@gmail.com>
2017-04-04 13:10:41 -0600

    Clarifies user model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,9 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   context "relationships" do
     it { should have_many(:links) }
+  end
+
+  context "validations" do
     it { should validate_uniqueness_of(:email) }
     it { should validate_presence_of(:email) }
   end

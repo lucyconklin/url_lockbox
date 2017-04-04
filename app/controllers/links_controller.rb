56f89208dbb702bb21a9e08fabbb525581caf892
Lucy Conklin <lucy.conklin@gmail.com>
2017-04-04 17:01:08 -0600

    Passing tests for edit spec including sad paths

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -15,6 +15,21 @@ class LinksController < ApplicationController
     end
   end
 
+  def edit
+    @link = Link.find(params[:id])
+  end
+
+  def update
+    @link = Link.find(params[:id])
+    if @link.update(link_params)
+      @link.save
+      redirect_to links_path
+    else
+      flash[:error] = @link.errors.full_messages
+      redirect_to edit_link_path(@link)
+    end
+  end
+
   private
 
   def link_params
--- app/models/link.rb
+++ app/models/link.rb
@@ -5,18 +5,7 @@ class Link < ActiveRecord::Base
   validates :url, presence: true
   validates :url, :url => true
 
-  # validate :url_is_valid
   validates_uniqueness_of :url, scope: :user_id
 
   belongs_to :user
-
-  # def url_is_valid
-  #   url ||= ' '
-  #   encoded_url = url.gsub(' ','%20')
-  #   if URI::parse(encoded_url).host != nil
-  #     return true
-  #   else
-  #     errors.add(:url, "is invalid")
-  #   end
-  # end
 end

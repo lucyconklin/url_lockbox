fae332b4bcb4e3b1fcd1a496306c60b6e18c4422
Lucy Conklin <lucy.conklin@gmail.com>
2017-04-04 11:31:13 -0600

    Adds all routes and controller actions for signup

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   root to: 'sessions#new'
 
   resources :links, only: [:index]
-  # resources :users, only: [:new]
+  resources :users, only: [:create]
+  get '/signup', to: 'users#new', as: 'signup'
+
   get '/login', to: 'sessions#new', as: 'login'
-  get '/signup', to: 'sessions#new', as: 'signup'
 
   namespace :api do
     namespace :v1 do

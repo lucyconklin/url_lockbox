f0afbac686fd0d51ec3bfcb5b90d6d3c449a3676
Lucy Conklin <lucy.conklin@gmail.com>
2017-04-17 10:22:04 -0600

    Add quirks mode for json

--- spec/requests/api/v1/test_endpoint_for_service_spec.rb
+++ spec/requests/api/v1/test_endpoint_for_service_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 describe "Test Endpoint" do
   it "test endpoint returns 'hello world' as json" do
     get '/api/v1/test'
-    result = response.body
+
+    result = JSON.parse(response.body, :quirks_mode => true)
 
     expect(result).to eq('Hello World!')
   end

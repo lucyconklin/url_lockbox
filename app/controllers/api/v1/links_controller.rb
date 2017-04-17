ca1222bfab56ec0de082d63542e9b9ddb69514f9
Lucy Conklin <lucy.conklin@gmail.com>
2017-04-17 17:40:52 -0600

    Descending links

--- app/controllers/api/v1/links_controller.rb
+++ app/controllers/api/v1/links_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::LinksController < ApplicationController
 
   def index
-    links = Link.order(read_count: :asc)
+    links = Link.order(read_count: :desc)
     render json: links.to_json
   end
 
